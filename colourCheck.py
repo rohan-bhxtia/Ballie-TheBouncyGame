7edd20cc898d13a5eb49c963155a5f2820c469ed
Rohan ! <149778795+rohan-bhxtia@users.noreply.github.com>
2025-10-13 22:38:37 +0530

    Update colourCheck.py

--- colourCheck.py
+++ colourCheck.py
@@ -14,7 +14,7 @@ def hex_to_rgb(hex_color):
     return tuple(int(hex_color[i:i+2], 16) for i in (0, 2, 4))
 
 # Example colors
-color1 = hex_to_rgb("#FF5733")  # orange
+color1 = hex_to_rgb("#FF5850")  # orange
 color2 = hex_to_rgb("#00FF00")  # green
 color3 = hex_to_rgb("#0000FF")  # blue
 
