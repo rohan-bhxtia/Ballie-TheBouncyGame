46723638de20e112cad8d303029822d65cb46976
Rohan ! <149778795+rohan-bhxtia@users.noreply.github.com>
2025-10-26 23:35:40 +0530

    Update colourCheck.py

--- colourCheck.py
+++ colourCheck.py
@@ -4,7 +4,7 @@ import pygame
 pygame.init()
 
 # Set up canvas
-WIDTH, HEIGHT = 600, 700
+WIDTH, HEIGHT = 600, 800
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Hex Color Test")
 
